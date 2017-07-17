317c8756c6118df1b374437037044ac9f8343d31
rchan <shihping.chan@gmail.com>
2017-07-17 14:23:16 +0800

    use external database file

--- service.py
+++ service.py
@@ -1,17 +1,12 @@
 import falcon
 from jinja2 import Template
 import json
+import yaml
 
-INSTANCES = {
-    '192.168.125.200': { 'instance_id': 'iid-e08cd154ca7a9491',
-                         'local_hostname': 'cloud-9491',
-                         'ip_address': '192.168.125.200'
-    },
-    '192.168.125.201': { 'instance_id': 'iid-95a39c8bdca53915',
-                         'local_hostname': 'cloud-3915',
-                         'ip_address': '192.168.125.201'
-    }
-}
+with open('database.yml') as dbase:
+    config_db = yaml.load(dbase)
+
+INSTANCES = config_db['instances']    
 
 METADATA_JSON_J2 = '''{
   "uuid" : "{{ instance_id }}",
@@ -20,8 +15,8 @@ METADATA_JSON_J2 = '''{
   "network_config": { "content_path": "/content/0000" }
 }'''
 
-NETWORK_0000_J2 = '''auto eth0
-iface eth0 inet static
+NETWORK_0000_J2 = '''auto {{ if_name }}
+iface {{ if_name }} inet static
     address {{ ip_address }}
     network 192.168.125.0
     netmask 255.255.255.0
@@ -46,6 +41,27 @@ class MetaData:
             print("<7>", my_key, INSTANCES[my_key])
             resp.body = METADATA_TMPL.render(INSTANCES[my_key])
             resp.append_header('Content-Type', 'application/json')
+        else:
+            resp.status = HTTP_404
+
+class Ec2MetaData:
+    def on_get(self, req, resp, key):
+        print("<7> Ec2MetaData", req.remote_addr, req.access_route, key)
+        my_key = req.access_route[0]
+        if my_key in INSTANCES:
+            print("<7>", my_key, INSTANCES[my_key])
+            key_0 = key.replace('-', '_')
+            resp.body = INSTANCES[my_key].get(key_0, "")
+        else:
+            resp.status = HTTP_404
+
+class Ec2MetaDataRoot:
+    def on_get(self, req, resp):
+        print("<7> Ec2MetaData", req.remote_addr, req.access_route)
+        my_key = req.access_route[0]
+        if my_key in INSTANCES:
+            print("<7>", my_key, INSTANCES[my_key])
+            resp.body = "instance-id\nlocal-hostname\n"
         else:
             resp.status = HTTP_404
 
@@ -83,13 +99,35 @@ class NullData:
         else:
             resp.status = HTTP_404
 
+class OpenStackRoot:
+    def on_get(self, req, resp):
+        print("<7> OpenStackRoot", req.remote_addr, req.access_route)
+        my_key = req.access_route[0]
+        if my_key in INSTANCES:
+            print("<7>", my_key, INSTANCES[my_key])
+            resp.body = "latest"
+        else:
+            resp.status = HTTP_404
+
+class Version:
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        print("<7> Version", req.remote_addr, req.access_route)
+        resp.body = "v0.0.1 2017-07-17"
+
             
             
 with open('user-data') as fi:
     USER_DATA =  fi.read()
 
 api = falcon.API()
+api.add_route('/openstack', OpenStackRoot())
 api.add_route('/openstack/latest/meta_data.json', MetaData())
 api.add_route('/openstack/latest/user_data', UserData())
 api.add_route('/openstack/content/{filename}', ContentData())
 api.add_route('/latest/meta-data', NullData())
+
+api.add_route('/2009-04-04/meta-data', Ec2MetaDataRoot())
+api.add_route('/2009-04-04/meta-data/{key}', Ec2MetaData())
+api.add_route('/2009-04-04/user-data', UserData())
+api.add_route('/api/v1/version', Version())
